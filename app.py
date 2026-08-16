a77f5fcc4cdcd73f9531b7c358ceaa08e60dd9d7
larissamariana-sudo <larissamariana@gmail.com>
2026-08-16 12:25:21 -0300

    Refactor Streamlit app structure and content

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ elif menu == "🎟️ Inscrições (Eventos)":
     
     with st.form("form_inscricao"):
         evento_escolhido = st.selectbox("Selecione o Evento:", [
-            "Jornada Científica do Curso de Fisioterapia (Gratuito - Apenas "Estudante PUC Goiás", "Docente - PUC Goiás")", 
+            "Jornada Científica do Curso de Fisioterapia (Gratuito - Apenas "Estudante PUC Goiás")", 
+            "Jornada Científica do Curso de Fisioterapia (Gratuito - Apenas "Docente - PUC Goiás")", 
             "Jornada Científica do Curso de Fisioterapia (Pago - R$ 5,00 - "Estudante Externo")", 
             "Jornada Científica do Curso de Fisioterapia (Pago - R$ 10,00 - "Docente Externo", "Profissional")", 
             "Minicurso Prático: Massagem Terapêutica (Pago - R$ 30,00)", 
