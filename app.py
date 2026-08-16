8e0a2db98ddb5bdb4fe6a365852ad302180dbde5
larissamariana-sudo <larissamariana@gmail.com>
2026-08-16 17:21:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ st.set_page_config(
 
 # --- ESTILIZAÇÃO DO CABEÇALHO ---
 st.markdown("""
-    <div style='background-color: #004225; padding: 25px; border-radius: 10px; text-align: center; color: white;'>
+    # Para exibir uma imagem centralizada ou ajustada
+st.image("logo_jornada.png", caption="Portal Acadêmico - PUC Goiás", use_container_width=True)
+<div style='background-color: #004225; padding: 25px; border-radius: 10px; text-align: center; color: white;'>
         <h1 style='margin:0; font-size: 26px;'>Eventos Científicos na Saúde, Humanidades e Áreas Afins — PUC Goiás</h1>
         <p style='margin:5px 0 0 0; font-size: 15px;'>Portal de Eventos, Submissões, Anais e Certificação DOI</p>
     </div>
@@ -29,12 +31,23 @@ menu = st.sidebar.selectbox("Navegue pelo Portal:", [
 if menu == "🏠 Início / Sobre":
     st.subheader("Sobre o Portal Acadêmico")
     st.write("Bem-vindo à plataforma de gestão acadêmica e científica para eventos da PUC Goiás.")
+        st.write("Bem-vindo ao Portal de Eventos Científicos FST PUC Goiás. Este portal foi desenvolvido para centralizar a gestão acadêmica e científica dos eventos dos cursos.
+       Aqui, estudantes, professores, pesquisadores e profissionais encontram um espaço integrado para: 
+Inscrições em jornadas, simpósios, palestras e minicursos;
+
+Submissão de Trabalhos (Resumos Simples, Expandidos e Artigos Completos) divididos por eixos temáticos;
+
+Repositório Oficial de Anais com edições anteriores publicadas com ISBN.
+
+Emissão Opcional de DOI Individual para artigos aprovados, garantindo maior visibilidade acadêmica (Lattes e Pós-Graduação);
+
+Navegue pelo menu lateral para realizar suas inscrições, conferir as normas vigentes e acompanhar a programação dos nossos eventos!")
 
 # --- 2. INSCRIÇÕES ---
 elif menu == "🎟️ Inscrições (Eventos)":
     st.subheader("🎟️ Inscrição na Jornada Científica do Curso de Fisioterapia")
     st.write("""
-        As inscrições no evento para banca examinadora, estudantes e docentes da PUC Goiás é gratuita.
+        As inscrições no evento para banca examinadora, estudantes e docentes da PUC Goiás, é gratuita.
         
         As inscrições no evento para externos (estudantes, docentes e profissionais - categorias extenas) é confirmada após o pagamento da taxa de inscrição e envio do comprovante de pagamento através do formulário integrado.
     """)    
